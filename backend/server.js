330b767184dddde1750065f5aa07b62e78c9f23d
Pupunfzx <beheraj542@gmail.com>
2026-04-14 17:11:52 +0530

    fix gps problem

--- backend/server.js
+++ backend/server.js
@@ -30,7 +30,7 @@ app.use(
     allowedHeaders: ["Content-Type", "Authorization"],
   }),
 );
-app.options("*", cors());
+app.options(/(.*)/, cors());
 app.use(express.json());
 
 // Serve OTA firmware uploads statically
